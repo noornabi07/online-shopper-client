78c81e73804c4204956efab9760be52351fe387d
noornabi07 <noornabiprogram07@gmail.com>
2024-02-05 18:39:44 +0600

    start from now oline shopper in start

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>Hello this is main Layout</h2>
+        </div>
+    );
+};
+
+export default MainLayout;

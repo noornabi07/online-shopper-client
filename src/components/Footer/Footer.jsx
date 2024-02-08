f69ad6d3470285104fa560b8b2cd19547f3f3b0f
noornabi07 <noornabiprogram07@gmail.com>
2024-02-08 17:15:44 +0600

    footer section design complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import footerLogo from '../assets/all-products/logo_big.png'
+import { FaFacebookF, FaInstagram, FaWhatsapp, FaTwitter } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer Section</h2>
+            <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
+                <div className='flex justify-center items-center gap-3'>
+                    <img src={footerLogo} alt="" />
+                    <h2 className='uppercase font-bold text-4xl text-red-300'>shopper</h2>
+                </div>
+                <nav className="grid grid-flow-col gap-4">
+                    <a className=" hover:-mt-3 font-semibold cursor-pointer hover:duration-300 hover:text-red-300">Company</a>
+                    <a className=" hover:-mt-3 font-semibold cursor-pointer hover:duration-300 hover:text-red-300">Products</a>
+                    <a className=" hover:-mt-3 font-semibold cursor-pointer hover:duration-300 hover:text-red-300">Offices</a>
+                    <a className=" hover:-mt-3 font-semibold cursor-pointer hover:duration-300 hover:text-red-300">About</a>
+                    <a className=" hover:-mt-3 font-semibold cursor-pointer hover:duration-300 hover:text-red-300">Contact</a>
+                </nav>
+                <nav>
+                    <div className="grid grid-flow-col gap-4">
+                        <Link to='https://www.facebook.com/mdnurnobiislam.suhayel.1'><FaFacebookF className='text-2xl hover:scale-105 hover:duration-300 hover:text-red-400  font-semibold text-gray-600 cursor-pointer'></FaFacebookF ></Link>
+                        <Link to='https://www.instagram.com/musicpromoter33/'><FaInstagram className='text-2xl hover:scale-105 hover:duration-300 hover:text-red-400 font-semibold text-gray-600 cursor-pointer'></FaInstagram></Link>
+                        <Link to='https://twitter.com/MDNurna56749656'><FaTwitter className='text-2xl hover:scale-105 hover:duration-300 hover:text-red-400 font-semibold text-gray-600 cursor-pointer'></FaTwitter></Link>
+                        <Link to='+8801863702344'><FaWhatsapp className='text-2xl hover:scale-105 hover:duration-300 hover:text-red-400 font-semibold text-gray-600 cursor-pointer'></FaWhatsapp></Link>
+                        
+                    </div>
+                </nav>
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by Noornabi Islam</p>
+                </aside>
+            </footer>
         </div>
     );
 };
--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -6,14 +6,14 @@ const NewsLetter = () => {
     return (
         <Container>
             <div className='newscolor rounded-xl my-5'>
-                <div className="hero h-[400px]">
+                <div className="hero md:h-[400px]">
                     <div className="hero-content text-center">
                         <div className="">
                             <h1 className="text-5xl font-semibold">Get Exclusive Offers On Your Email</h1>
-                            <p className="py-6 text-lg font-semibold text-gray-500">Subscribe to our newsletter and stay updated</p>
+                            <p className="md:py-6 text-lg font-semibold text-gray-500">Subscribe to our newsletter and stay updated</p>
                             {/* newsletter input letter */}
                             <div className='flex items-center justify-center'>
-                                <input type="text" placeholder="Type here" className="input input-primary w-full max-w-sm" />
+                                <input type="email" placeholder="Your Email id" className="input input-primary w-full max-w-sm" />
                                 <button className="py-3 -ml-7 rounded-3xl px-8 bg-black text-white">Subscribes</button>
                             </div>
                         </div>
